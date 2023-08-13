718b784668c3ff01de9d4ef84f25d0e962cb9e96
Hwangsangjin <hwangsangjin88@gmail.com>
2023-08-13 17:44:03 +0900

    함수

--- Project/main.cpp
+++ Project/main.cpp
@@ -6,41 +6,26 @@
 
 using namespace std;
 
+// 함수 (선언과 정의 분리 가능)
+int Add(int a, int b)
+{
+	return a + b; // 반환값
+}
+
+// 반환 자료형이 지정되지 않았음 (void)
+void Add(int a, int b, int* c)
+{
+	*c = a + b;
+}
+
 int main()
 {
-    int a = 123;
+	cout << Add(1, 2) << endl;
 
-    // address of 주소 연산자 & ampersand
-    cout << a << " " << &a << endl;
+	int sum;
+	Add(4, 5, &sum);
 
-    int* b = &a; // b에 a의 주소 대입
-
-    // deference operator (역참조 연산자)
-    cout << *b << endl;
-
-    *b = 567;
-
-    cout << a << " " << b << " " << *b << endl;
-
-    // 포인터 자체의 주소 크기와 자료형의 크기 (주소의 크기는 항상 동일하다.)
-    double* c = nullptr; // 아무 주소도 가리키고 있지 않다는 의미로 초기화, 0도 많이 사용
-
-    cout << sizeof(int) << " " << sizeof(double) << endl;
-    cout << sizeof(int*) << " " << sizeof(double*) << endl;
-    cout << sizeof(b) << " " << sizeof(c) << endl;
-
-    // 포인터 연산과 배열
-    // size_t (여기서는 주소를 10진수로 변환 용도)
-    cout << sizeof(size_t) << endl; // 8
-    cout << size_t(b) << " " << size_t(b + 1) << " " << size_t(b + 2) << endl;
-    cout << size_t(c) << " " << size_t(c + 1) << " " << size_t(c + 2) << endl;
-
-    // 문자열
-    char my_str[] = { 'h', 'e', 'l', 'l', 'o' }; // "Hello"
-
-    char* ptr = my_str; // 배열의 이름은 포인터
-
-    cout << *(ptr + 3) << endl;
+	cout << sum << endl;
 
 	return 0;
 }
