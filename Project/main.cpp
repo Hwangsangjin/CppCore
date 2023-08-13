dd0d91ffdbec19c7806737a73edf321fb138a8f2
Hwangsangjin <hwangsangjin88@gmail.com>
2023-08-13 11:43:06 +0900

    헬로우 월드

--- Project/main.cpp
+++ Project/main.cpp
@@ -1,10 +1,17 @@
-#include <iostream>
+﻿/*
+	홍정모 연구소 https://honglab.co.kr/
+*/
 
-using namespace std;
+// 주석(comment)
+
+#include <iostream>		// iostream이라는 헤더를 포함
+
+using namespace std;	// 네임스페이스
 
 int main()
 {
 	cout << "Hello, World!" << endl;
+	//printf("Hello, World!\n");
 
 	return 0;
 }
